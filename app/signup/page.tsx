bfb8478fd0916a9d8113ef080641e29ee9c86a99
Charlie Rodiger <charlie@charlies-mbp.mynetworksettings.com>
2024-05-09 18:29:31 -0400

    fix redirect?

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <SignUp />
+      <SignUp afterSignUpUrl="/home" />
     </div>
   )
 }
