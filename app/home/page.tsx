c7cbbd564b7a4dd2d50f10ada4cfd9273dfaee3d
Charlie Rodiger <charlie@charlies-mbp.mynetworksettings.com>
2024-05-19 14:59:47 -0400

    new homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6">
           <div className="hidden md:block">
-            <div className="grid gap-6">
+            <div className="grid grid-cols-2 gap-6">
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">Spellbook</h1>
